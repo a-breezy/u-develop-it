8dc1e4d7bee2ecb50832ddebd11741c20f8c61b2
a.breezy <ambrose.wilkins@gmail.com>
2021-12-23 09:32:42 -0500

    db.query with err handling

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+	{
+		host: "localhost",
+		// Your mysql username,
+		user: process.env.DB_USER,
+		// your mysql password
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+	},
+	console.log("Connected to the election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
